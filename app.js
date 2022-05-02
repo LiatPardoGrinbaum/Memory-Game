df532ee32ba7bd80a7cc122c3c01866456466781
LiatPardoGrinbaum <pardo.liat@gmail.com>
2022-05-02 12:08:49 +0300

    update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let h4 = {
+/* let h4 = {
   id: "4h",
   img: "asdf.jpg",
 }[(h4, h5)];
@@ -8,3 +8,86 @@ function randomCard() {
 function randomPlace(param) {
   //
 }
+ */
+// const cells = document.querySelectorAll(".cell");
+// console.log(cells);
+
+const arrObj = [
+  {
+    id: "club3",
+    source: "./images/club_3.png",
+  },
+  {
+    id: "club3",
+    source: "./images/club_3.png",
+  },
+  {
+    id: "club4",
+    source: "./images/club_4.png",
+  },
+  {
+    id: "club4",
+    source: "./images/club_4.png",
+  },
+  {
+    id: "club5",
+    source: "./images/club_5.png",
+  },
+  {
+    id: "club5",
+    source: "./images/club_5.png",
+  },
+  {
+    id: "diamond_1",
+    source: "./images/diamond_1.png",
+  },
+  {
+    id: "diamond_1",
+    source: "./images/diamond_1.png",
+  },
+  {
+    id: "club_queen",
+    source: "./images/club_queen.png",
+  },
+  {
+    id: "club_queen",
+    source: "./images/club_queen.png",
+  },
+  {
+    id: "club_king",
+    source: "./images/club_king.png",
+  },
+  {
+    id: "club_king",
+    source: "./images/club_king.png",
+  },
+];
+
+const backImages = document.querySelectorAll(".back");
+
+const random = Math.floor(Math.random() * backImages.length);
+
+function setCards() {
+  backImages.forEach((div) => {
+    const img = document.createElement("img");
+    const random = Math.floor(Math.random() * arrObj.length);
+    img.src = arrObj[random].source;
+    div.setAttribute("id", arrObj[random].id);
+    div.appendChild(img);
+    arrObj.splice(random, 1);
+  });
+}
+setCards();
+
+const cards = document.querySelector(".cards");
+// console.log(cards);
+
+cards.addEventListener("click", function (e) {
+  console.log(e.target);
+  e.target.style.display = "none";
+
+  // if (e.target !== e.currentTarget) {
+  // console.log(e.target);
+
+  // }
+});
